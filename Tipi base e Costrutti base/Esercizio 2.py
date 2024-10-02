ac80560ea0d711a9177f1e7a69a9bc78f87422b9
Daniele Lombardi <lombardidaniele2003@gmail.com>
2024-10-02 13:53:40 +0200

    Delete Tipi base e Costrutti base directory

--- Tipi base e Costrutti base/Esercizio 2.py
+++
@@ -1,6 +0,0 @@
-# Scrivere un programma che prenda un valore inserito dall’utente e mostri a video il tipo che assume la variabile in cui viene inserito.
-# 
-# Aiutativei con la funzione type()
-
-a = input('Inserisci qualcosa: ')
-print('Hai inserito qualcosa di tipo: ',type(a))
