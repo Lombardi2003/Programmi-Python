c5ed85c9fe596623a31622083c86019579932339
Lombardi2003 <lombardidaniele2003@gmail.com>
2024-10-02 13:48:52 +0200

    Tipi base e Costrutti base

--- Tipi base e Costrutti base/Esercizio 7.py
+++ Tipi base e Costrutti base/Esercizio 7.py
@@ -5,7 +5,15 @@
 
 a = input("Inserisci un testo lungo: ")
 caratteri = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','w','x','y','z']
+f = []
 
 print("\nStampa delle frequenze non ordinate: ")
 for c in caratteri:
     print(c,' = ',a.count(c))
+    f.append((c,a.count(c)))
+
+frequenze_ordinate = sorted(f, key=lambda x: x[1], reverse=True)
+
+print("\nStampa delle frequenze ordinate: ")
+for c, freq in frequenze_ordinate:
+    print(c, ' = ', freq)
